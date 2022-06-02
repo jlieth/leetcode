c2f9ae3fdcb23cc672cbf58e57b0351f504771dd
jlieth <jlieth@users.noreply.github.com>
2022-06-02 04:54:48 +0200

    #867 easy python

---
+++ transpose-matrix/solution.py
@@ -0,0 +1,18 @@
+"""
+https://leetcode.com/problems/transpose-matrix/
+
+Runtime: 84 ms, faster than 68.17% of Python3 online submissions for Transpose Matrix.
+Memory Usage: 14.8 MB, less than 55.33% of Python3 online submissions for Transpose Matrix.
+"""
+from typing import List
+
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        m = len(matrix)
+        n = len(matrix[0])
+            
+        return [
+            [matrix[row][col] for row in range(m)] for col in range(n)
+        ]
+
