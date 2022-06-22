bbedceb2d52c38692d53dbc92e418f09db850db8
jlieth <jlieth@users.noreply.github.com>
2022-06-22 15:00:08 +0200

    #1480 easy python formatting

--- running-sum-of-1d-array/solution.py
+++ running-sum-of-1d-array/solution.py
@@ -7,7 +7,7 @@ Memory Usage: 14.1 MB, less than 73.38% of Python3 online submissions for Runnin
 from itertools import accumulate
 from typing import List
 
+
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         return list(accumulate(nums))
-
